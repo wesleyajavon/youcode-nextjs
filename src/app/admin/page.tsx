80bee06c904d515f0350cde2ed343d5ca7c98c23
wesleyajavon <wesleyajavon2203@hotmail.com>
2025-07-01 20:23:32 -0400

    T4 - in progress

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import { getRequiredAuthSession } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+
+export default async function AdminPage() {
+
+    return (
+        <div>TODO</div>
+    )
+}
