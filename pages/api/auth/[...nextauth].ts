f548d45e390b8146805f6b1bb4a949907b45eb33
wesleyajavon <wesleyajavon2203@hotmail.com>
2025-07-02 23:14:22 -0400

    OAuth - google, facebook, github implemented

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,8 @@ import { prisma } from '@/lib/prisma';
 import NextAuth, { AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -14,6 +16,14 @@ export const authOptions: AuthOptions = {
       clientId: env.GITHUB_ID,
       clientSecret: env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: env.FACEBOOK_CLIENT_ID,
+      clientSecret: env.FACEBOOK_CLIENT_SECRET,
+    }),
   ],
   callbacks: {
     session({ session, user }) {
@@ -21,6 +31,10 @@ export const authOptions: AuthOptions = {
       session.user.image = user.image;
       return session;
     },
+    async redirect({ url, baseUrl }) {
+      // Default redirect after login
+      return "/" // or any custom route
+    }
   },
 };
 
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,11 +1,15 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
- 
+
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     GITHUB_ID: z.string().min(1),
     GITHUB_SECRET: z.string().min(1),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
+    FACEBOOK_CLIENT_ID: z.string().min(1),
+    FACEBOOK_CLIENT_SECRET: z.string().min(1),
   },
   client: {
     // Nothing here yet
