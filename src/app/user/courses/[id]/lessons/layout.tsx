7ecd90c3374bf50ce0011820c762e519337bc65e
wesleyajavon <wesleyajavon2203@hotmail.com>
2025-07-12 19:45:48 -0400

    upd: props layout page

--- src/app/user/courses/[id]/lessons/layout.tsx
+++ src/app/user/courses/[id]/lessons/layout.tsx
@@ -2,9 +2,9 @@ import { getRequiredAuthSession } from "@/lib/auth"
 import { prisma } from "@/lib/prisma"
 import { redirect } from "next/navigation"
 
-export default async function LessonLayout({ children, params }: { children: React.ReactNode, params: { id: string } }) {
+export default async function LessonLayout({ children, params }: { children: React.ReactNode, params: Promise<{ id: string }> }) {
   const session = await getRequiredAuthSession()
-  const courseId = params.id
+  const courseId = (await params).id
 
   // Vérifie si l'utilisateur a rejoint ce cours
   const courseOnUser = await prisma.courseOnUser.findUnique({
